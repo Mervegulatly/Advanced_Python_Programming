c2b08c22e516301c87f92f22d3799ee999d2bf91
Merve Gül ATALAY <mervegulatly7@gmail.com>
2026-04-25 11:38:03 +0300

    @classmethods created

---
+++ Hafta2/Part1/class-methods.py
@@ -0,0 +1,66 @@
+# # @classmethod  # @classmethod yazıyorsa bu method nesneye değil sınıfın kendisine hizmet ediyor demektir. Parametre olarak self almaz.
+# # cls yi parametre olarak alır.
+
+# class Carditem():
+#     display_rate = 0.2
+#     item_count = 0
+
+#     def display_count(cls):
+#         return (f"{cls.itemcount} tane ürün sepete eklendi.")
+    
+#     def __init__(self, name, price, quantity):
+#         self.name = name
+#         self.price = price
+#         self.quantity = quantity
+#         Carditem.item_count += 1
+        
+
+# item1 = Carditem("Telefon", 10000, 2)
+# item2 = Carditem("Bilgisayar", 70000, 3)
+# item3 = Carditem("Tablet", 20000, 3)
+
+# print(Carditem.display_count())
+# print(Carditem.display_rate())
+
+
+class CartItem:
+
+    discount_rate=0.2
+    item_count=0
+
+    @classmethod
+    def display_count(cls):
+        return f"{cls.item_count} tane ürün sepete eklendi."
+    
+    @classmethod
+    def create_item(cls, data_str):
+        name, price, quantity=data_str.split(",")
+        return cls(name, price, quantity)
+
+    def __init__(self, name, price, quantity):
+        #instance attributes
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+        CartItem.item_count += 1
+
+    #instance methods
+    def calculate_total(self):
+        return self.price * self.quantity
+    
+    def appy_discount(self):
+        self.price=self.price * (1-CartItem.discount_rate)
+        #return self.price
+
+print(CartItem.display_count())
+
+item1=CartItem("Telefon",80000,2)
+item2=CartItem("Bilgisayar",90000,1)
+item3=CartItem("Tablet",20000,2)
+
+print(CartItem.display_count())
+
+CartItem.create_item("Mouse,2000,3")
+
+print(CartItem.display_count())
+
