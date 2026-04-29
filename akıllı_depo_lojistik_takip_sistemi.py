9f7adf2ad996324f5c6de5b55cdda1c9432e6d2f
Merve Gül ATALAY <mervegulatly7@gmail.com>
2026-04-29 11:58:50 +0300

    Value Error added for the stok kod and calculate shipping added

--- akıllı_depo_lojistik_takip_sistemi.py
+++ akıllı_depo_lojistik_takip_sistemi.py
@@ -28,11 +28,30 @@ class BaseProduct:
         else:
             raise ValueError("Ağırlık 0'dan küçük olamaz!")
 
+    @property
+    def stok_kodu(self):
+        return self._stok_kod
+    
+    @stok_kodu.setter
+    def stok_kodu(self, value):
+        if not (isinstance(value, str) and value.startswith("STK-") and value[4:].isdigit() and len(value) == 8):
+            raise ValueError("Stok kodu 'STK-1234' formatında olmalıdır")
+        self._stok_kod = value
+
+
+    def calculate_shipping(self):
+        shipping = self._price + (self._agirlik * self._stok_miktar)
+        return shipping
+
+
 class ElectronicProduct(BaseProduct):
 
     def __init__(self, _stok_kod, _price, _agirlik, _stok_miktar, warranty_period):
         super().__init__(_stok_kod, _price, _agirlik, _stok_miktar)
         self.warranty_period = warranty_period
+
+    def calculate_shipping(self):
+        return super().calculate_shipping()
 
 class FoodProduct(BaseProduct):
 
@@ -40,11 +59,30 @@ class FoodProduct(BaseProduct):
         super().__init__(_stok_kod, _price, _agirlik, _stok_miktar)
         self.expiry_date = expiry_date
 
+    def calculate_shipping(self):
+        return super().calculate_shipping()
+
 class HazardousProduct(BaseProduct):
 
     def __init__(self, _stok_kod, _price, _agirlik, _stok_miktar, safety_level):
         super().__init__(_stok_kod, _price, _agirlik, _stok_miktar)
         self.safety_level = safety_level
+
+def calculate_shipping(self):
+        base_shipping = super().calculate_shipping()
+
+        multipliers = {
+            "LOW": 1.0,
+            "MEDIUM": 1.1,
+            "HIGH": 1.25
+        }
+
+        return base_shipping * multipliers.get(self.safety_level, 1.0)
+
+class LogisticsProcessor(BaseProduct):
+    def calculate_shipping(self):
+        LogisticsProcessorPolimorphism = super().calculate_shipping()
+        return print(f"Logistic'deki polimorphism : {LogisticsProcessor}")
 
 class InventoryManager:
     
