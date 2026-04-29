ed69c82bbabcf90544b2c417792b2eae814f4e72
Merve Gül ATALAY <mervegulatly7@gmail.com>
2026-04-29 13:36:54 +0300

    property added and LogisticsProcessor fixed

--- akıllı_depo_lojistik_takip_sistemi.py
+++ akıllı_depo_lojistik_takip_sistemi.py
@@ -1,10 +1,11 @@
 class BaseProduct:
 
     def __init__(self, _stok_kod, _price, _agirlik, _stok_miktar):
-        self._stok_kod = _stok_kod
-        self._price = _price
-        self._agirlik = _agirlik
-        self._stok_miktar = _stok_miktar
+        self.stok_kod = _stok_kod
+        self.price = _price
+        self.agirlik = _agirlik
+        self.stok_miktar = _stok_miktar
+
 
     @property
     def price(self):
@@ -38,9 +39,20 @@ class BaseProduct:
             raise ValueError("Stok kodu 'STK-1234' formatında olmalıdır")
         self._stok_kod = value
 
+    @property
+    def stok_miktar(self):
+        return self._stok_miktar
+
+    @stok_miktar.setter
+    def stok_miktar(self, value):
+        if value < 0:
+            raise ValueError("Stok negatif olamaz")
+        self._stok_miktar = value
+
+
 
     def calculate_shipping(self):
-        shipping = self._price + (self._agirlik * self._stok_miktar)
+        shipping = self._agirlik * self._stok_miktar
         return shipping
 
 
@@ -51,7 +63,7 @@ class ElectronicProduct(BaseProduct):
         self.warranty_period = warranty_period
 
     def calculate_shipping(self):
-        return super().calculate_shipping()
+        return super().calculate_shipping() * 1.2
 
 class FoodProduct(BaseProduct):
 
@@ -60,7 +72,7 @@ class FoodProduct(BaseProduct):
         self.expiry_date = expiry_date
 
     def calculate_shipping(self):
-        return super().calculate_shipping()
+        return super().calculate_shipping() * 1.1
 
 class HazardousProduct(BaseProduct):
 
@@ -68,26 +80,46 @@ class HazardousProduct(BaseProduct):
         super().__init__(_stok_kod, _price, _agirlik, _stok_miktar)
         self.safety_level = safety_level
 
-def calculate_shipping(self):
-        base_shipping = super().calculate_shipping()
-
-        multipliers = {
-            "LOW": 1.0,
-            "MEDIUM": 1.1,
-            "HIGH": 1.25
-        }
-
-        return base_shipping * multipliers.get(self.safety_level, 1.0)
-
-class LogisticsProcessor(BaseProduct):
     def calculate_shipping(self):
-        LogisticsProcessorPolimorphism = super().calculate_shipping()
-        return print(f"Logistic'deki polimorphism : {LogisticsProcessor}")
+            base_shipping = super().calculate_shipping()
+
+            multipliers = {
+                "LOW": 1.0,
+                "MEDIUM": 1.1,
+                "HIGH": 1.25
+            }
+
+            return base_shipping * multipliers.get(self.safety_level, 1.0)
+
+class LogisticsProcessor:
+    def __init__(self, manager):
+        self.manager = manager
+
+    def calculate_shipping(self, product):
+        return product.calculate_shipping()
+    
+    def kritik_stok(self, threshold):
+        for product in self.manager:
+            if product.stok_miktar < threshold:
+                yield product
 
 class InventoryManager:
     
     def __init__(self):
-        self.products = {}
+        self.products = []
+
+    def __iter__(self):
+        self._index = 0
+        return self
+
+    def __next__(self):
+        if self._index < len(self.products):
+            result = self.products[self._index]
+            self._index += 1
+            return result
+        else:
+            raise StopIteration
+
 
     def urun_ekle(self, urun_adi, adet):
         if urun_adi in self.products:
@@ -99,4 +131,8 @@ class InventoryManager:
         for urun, adet in self.products.items():
             print(f"{urun}: {adet} adet")
 
-            
+    def __str__(self):
+        return f"{self.stok_kodu} - {self.price}"
+
+manager = InventoryManager()
+manager.urun_ekle("Telefon", 23)
