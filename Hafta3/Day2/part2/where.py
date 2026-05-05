e231f2e3a84243558ef18f90640102b6daacf75c
Merve Gül ATALAY <mervegulatly7@gmail.com>
2026-05-05 15:36:23 +0300

    where, request cerated

---
+++ Hafta3/Day2/part2/where.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+db = sqlite3.connect("btk_akademi.db")
+cursor = db.cursor()
+
+sql = "SELECT * FROM urunler WHERE  fiyat > ? AND stok_adedi > 0"
+
+cursor.execute(sql, (50000,)) #Parametre ayrı da tanımlanabilrdi.
+
+sonuclar = cursor.fetchall()
+
+for s in sonuclar:
+    print(s)
+
+db.close()
+
